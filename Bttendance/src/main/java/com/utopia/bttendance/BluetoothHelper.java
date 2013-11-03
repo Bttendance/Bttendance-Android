ef4c3e4156f0d50e168fdd75c199178c6017406d
TheFinestArtist <contact@thefinestartist.com>
2013-11-03 22:08:49 +0900

    Bluetooth Search

--- Bttendance/src/main/java/com/utopia/bttendance/BluetoothHelper.java
+++ Bttendance/src/main/java/com/utopia/bttendance/BluetoothHelper.java
@@ -1,7 +1,67 @@
 package com.utopia.bttendance;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+
 /**
  * Created by TheFinestArtist on 2013. 11. 1..
  */
 public class BluetoothHelper {
+
+    public static final int REQUEST_ENABLE_BT = 21;
+    public static final int REQUEST_ENABLE_DISCOVERABILITY_BT = 22;
+    public static final int DISCOVERABILITY_BT_DURATION = 300;
+
+    public static BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    public static boolean isAvailable() {
+        if (mBluetoothAdapter == null)
+            return false;
+        return true;
+    }
+
+    public static void enable(Activity activity) {
+        if (!isAvailable())
+            return;
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    public static void enableWithUI() {
+        if (!isAvailable())
+            return;
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            mBluetoothAdapter.enable();
+        }
+    }
+
+    public static void disableWithUI() {
+        if (!isAvailable())
+            return;
+
+        if (mBluetoothAdapter.isEnabled()) {
+            mBluetoothAdapter.disable();
+        }
+    }
+
+    public static void enableDiscoverability(Activity activity) {
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVERABILITY_BT_DURATION);
+        activity.startActivityForResult(discoverableIntent, REQUEST_ENABLE_DISCOVERABILITY_BT);
+    }
+
+    public static void startDiscovery() {
+        if (!isAvailable())
+            return;
+
+        if (mBluetoothAdapter.isEnabled()) {
+            mBluetoothAdapter.startDiscovery();
+        }
+    }
+
 }
--- Bttendance/src/main/java/com/utopia/bttendance/MainActivity.java
+++ Bttendance/src/main/java/com/utopia/bttendance/MainActivity.java
@@ -1,15 +1,25 @@
 package com.utopia.bttendance;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -25,6 +35,12 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("Bttendance", "requestCode : " + requestCode + ", resultCode : " + resultCode);
+        Toast.makeText(getApplicationContext(),"requestCode : " + requestCode + ", resultCode : " + resultCode, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,16 +65,88 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener{
+
+        ArrayAdapter<String> mAdapter;
 
         public PlaceholderFragment() {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            Button on_dialog = (Button) rootView.findViewById(R.id.blue_on_dialog);
+            Button on = (Button) rootView.findViewById(R.id.blue_on);
+            Button off = (Button) rootView.findViewById(R.id.blue_off);
+            Button discover = (Button) rootView.findViewById(R.id.blue_discover);
+            Button find = (Button) rootView.findViewById(R.id.blue_find);
+            ListView list = (ListView) rootView.findViewById(R.id.blue_list);
+
+            on_dialog.setOnClickListener(this);
+            on.setOnClickListener(this);
+            off.setOnClickListener(this);
+            discover.setOnClickListener(this);
+            find.setOnClickListener(this);
+
+            mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
+            list.setAdapter(mAdapter);
+
+            // Register the BroadcastReceiver
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            getActivity().registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+
             return rootView;
+        }
+
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            getActivity().unregisterReceiver(mReceiver);
+        }
+
+        // Create a BroadcastReceiver for ACTION_FOUND
+        private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // Add the name and address to an array adapter to show in a ListView
+                    Log.e("Bttendance", device.getName() + "\n" + device.getAddress() + "\n" + device.getUuids());
+                    for (int i=0;i<device.getUuids().length;i++)
+                        Log.e("Bttendance", device.getUuids()[i].getUuid().toString());
+                    mAdapter.add(device.getName() + "\n" + device.getAddress() + "\n" + device.getUuids());
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+        };
+
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.blue_on_dialog:
+                    BluetoothHelper.enable(getActivity());
+                    return;
+                case R.id.blue_on:
+                    BluetoothHelper.enableWithUI();
+                    return;
+                case R.id.blue_off:
+                    BluetoothHelper.disableWithUI();
+                    return;
+                case R.id.blue_discover:
+                    BluetoothHelper.enableDiscoverability(getActivity());
+                    return;
+                case R.id.blue_find:
+                    mAdapter.clear();
+                    mAdapter.notifyDataSetChanged();
+                    Toast.makeText(getActivity(), "Start Discover", Toast.LENGTH_SHORT).show();
+                    BluetoothHelper.startDiscovery();
+                    return;
+                default:
+                    return;
+            }
         }
     }
 
